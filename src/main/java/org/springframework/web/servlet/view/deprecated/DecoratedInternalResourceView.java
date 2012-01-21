ec5cd6d33c4f65dc9cf5b853d9c53a989b63756a
jsimao <jsimao@ubuntu.ubuntu-domain>
2012-01-21 13:23:14 +0000

    ..

---
+++ src/main/java/org/springframework/web/servlet/view/deprecated/DecoratedInternalResourceView.java
@@ -0,0 +1,83 @@
+package org.springframework.web.servlet.view.deprecated;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.JspTemplateViewResolver;
+
+public class DecoratedInternalResourceView extends InternalResourceView implements DecoratedView {
+	protected String templatePath;
+	
+	protected String title;
+	
+	protected String layoutParam = JspTemplateViewResolver.LAYOUT_PARAM;
+	
+	protected String layoutParamValue = JspTemplateViewResolver.CANCEL_LAYOUT;
+	
+	
+	public DecoratedInternalResourceView() {
+	}
+
+	public String getTemplatePath() {
+		return templatePath;
+	}
+	
+	public void setTemplatePath(String templatePath) {
+		this.templatePath = templatePath;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLayoutParam() {
+		return layoutParam;
+	}
+
+	public void setLayoutParam(String layoutParam) {
+		this.layoutParam = layoutParam;
+	}
+
+	public String getLayoutParamValue() {
+		return layoutParamValue;
+	}
+
+	public void setLayoutParamValue(String layoutParamValue) {
+		this.layoutParamValue = layoutParamValue;
+	}
+	
+	@Override
+	protected void renderMergedOutputModel(
+			Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		if (useTemplate(request)) {
+			model.put("main", getUrl());
+			model.put("title", title);
+			System.out.println(this + "render: page:" + getUrl() + " template:" + templatePath);
+		}
+		super.renderMergedOutputModel(model, request, response);
+	}
+	
+	@Override
+	protected String prepareForRendering(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		if (useTemplate(request)) {
+			return templatePath;
+		} else {
+			return super.prepareForRendering(request, response);
+		}
+	}
+	
+	protected boolean useTemplate(HttpServletRequest request) {
+		return layoutParamValue==null || layoutParamValue.equals(request.getParameter(layoutParam));
+	}
+	
+	
+}
+
---
+++ src/main/webapp/WEB-INF/views/common/standard-alt.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<jsp:include page="header.jsp" />
+
+<span style="font-size:10pt;color:red">[Alternative template]</span>
+
+<jsp:include page="${viewUrl}" />
+
+<jsp:include page="footer.jsp" />
